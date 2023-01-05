1419507ea2c55f84176e952358f6ffeb0f866030
강영원 <66764608+kyw11@users.noreply.github.com>
2023-01-06 00:57:04 +0900

    Add files via upload

---
+++ B5_10871.java
@@ -0,0 +1,23 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class B5_10871 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		int[] arr = new int[N];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+
+		}
+		for (int i = 0; i < arr.length; i++) {
+			if (X > arr[i])
+				System.out.print(arr[i] + " ");
+		}
+
+	}
+
+}
