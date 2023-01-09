d78f1e09569cd4a42d9a546b96791bda92baec35
강영원 <66764608+kyw11@users.noreply.github.com>
2023-01-09 14:12:07 +0900

    Update B3_2562.java

--- B3_2562.java
+++ B3_2562.java
@@ -6,18 +6,22 @@ import java.util.Scanner;
 public class B3_2562 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		boolean[] arr = new boolean[31];
+		int[] student = new int[31];  //학생 수가 30명이라서 31이 맞아 
 		
-		Arrays.fill(arr, false);
-		
-		for(int i=0;i<28;i++) {
-			arr[sc.nextInt()] = true; 
+		for(int i=1;i<29;i++) {
+			int success = sc.nextInt();  // 제출한 사람 입력받아
+			student[success] =1;  // 제출한 사람 배열 값 1로 바꿔
+			
+			
 		}
-		for(int i=1;i<31;i++) {
-			if(!arr[i]) {
+		/////2023.01.09 방금 내가 for문 하나로 다 안되나? 이런 생각을 했는데 안돼
+		////왜냐면  첫 번째 for문은 입력을 받는 곳인데 그래서 28까지만 받았거든
+		////근데 출력 그러니까 제출 안한 사람을 찾으려면 student길이만큼 다 돌아야 하거든
+		for(int i=1;i<student.length;i++) {
+			if(student[i] != 1)
 				System.out.println(i);
-			}
 		}
+		
 		
 	}
 
