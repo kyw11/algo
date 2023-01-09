9506006c548cdbbd359887002dfd5698cd7babaa
강영원 <66764608+kyw11@users.noreply.github.com>
2023-01-09 13:36:17 +0900

    Add files via upload

--- B3_2562.java
+++ B3_2562.java
@@ -1,28 +1,23 @@
 package BOJ;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class B3_2562 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] arr = new int[9];
-		int max =0;
-		int index = 0;
+		boolean[] arr = new boolean[31];
 		
-	
-		max = arr[0];
-		for(int i=0;i<arr.length;i++) {
-			arr[i] = sc.nextInt();
+		Arrays.fill(arr, false);
+		
+		for(int i=0;i<28;i++) {
+			arr[sc.nextInt()] = true; 
 		}
-		for(int i=0;i<9;i++) {
-			if(arr[i]>max) {
-				max = arr[i];
-				index = i+1;
+		for(int i=1;i<31;i++) {
+			if(!arr[i]) {
+				System.out.println(i);
 			}
-			
 		}
-		System.out.println(max);
-		System.out.println(index);
 		
 	}
 
