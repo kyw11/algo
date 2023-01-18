ca6ae20055f65f509707b446dd68fc071a69ae84
강영원 <66764608+kyw11@users.noreply.github.com>
2023-01-18 19:41:05 +0900

    Add files via upload

---
+++ B5_11654.java
@@ -0,0 +1,12 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class B5_11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int ch = sc.next().charAt(0);
+		System.out.print(ch);
+	}
+
+}
