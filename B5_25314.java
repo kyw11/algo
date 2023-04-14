c731a3980c0cbfb248b4500ef2731a8780e61388
강영원 <66764608+kyw11@users.noreply.github.com>
2023-04-14 19:58:03 +0900

    Add files via upload

---
+++ B5_25314.java
@@ -0,0 +1,18 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class B5_25314 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int num = N/4;
+		for(int i=1;i<=num;i++) {  //입력 받은 배수 만큼 돌고
+			System.out.print("long ");
+			
+		}
+		System.out.println("int");
+			
+	}
+
+}
