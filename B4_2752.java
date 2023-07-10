497d32d0c830e598b7d262427fbeac740eaf510a
강영원 <66764608+kyw11@users.noreply.github.com>
2023-07-10 21:00:53 +0900

    Add files via upload

---
+++ B4_2752.java
@@ -0,0 +1,20 @@
+package BOJ;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B4_2752 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[3];
+		
+		for(int i =0;i<arr.length;i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		for(int i = 0;i<arr.length;i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+}
