c10a232ebf9b5541a6d168695adc4a4c9b176d3a
강영원 <66764608+kyw11@users.noreply.github.com>
2023-10-06 13:20:12 +0900

    Add files via upload

---
+++ B5_2743.java
@@ -0,0 +1,18 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class B5_2743 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int cnt =0;
+		
+		for(int i =0;i<str.length();i++) {
+			cnt ++;
+			
+		}
+		System.out.println(cnt);
+	}
+
+}
