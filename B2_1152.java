b1e3d5ed8a78186aba5bf8493a5f2255a54ae0e7
강영원 <66764608+kyw11@users.noreply.github.com>
2023-10-19 23:16:49 +0900

    Add files via upload

---
+++ B2_1152.java
@@ -0,0 +1,23 @@
+package BOJ;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class B2_1152 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		StringTokenizer st = new StringTokenizer(str," ");
+		System.out.println(st.countTokens());
+		//while(st.hasMoreElements()) {
+			//int
+		
+		//}
+		
+		
+		
+		
+		
+	}
+
+}
